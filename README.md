4af03f3b324ed38196975006dc4498f9dc6fd671
Radium <fog.code000@gmail.com>
2021-01-02 21:27:17 +0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# digital-logic-simulator
+A application to simulate digital logic using logic gates visually
+
---
+++ main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+from app.application import Application
+
+appl = Application()
+appl.start()
